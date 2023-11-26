739ded70b09e915d988f934058ab266b7e14d02c
greeenboi <suvan.gowrishanker.204@gmail.com>
2023-11-27 04:13:12 +0530

    Add header component to dashboard page

---
+++ app/Dashboard/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Image from 'next/image'
+import {
+    Button,
+
+} from '@chakra-ui/react'
+
+export function Header () {
+  return (
+    <nav className='w-full min-h-max mb-2 bg-slate-900 flex flex-row justify-between items-center'>
+        {/* <Image src={logo} alt="Sicu-aura" width={100} height={100} /> */}
+        <div>hello</div>
+        <div className='w-fit max-w-4xl pl-36 flex flex-row gap-4 justify-evenly'>
+            {/* <Image src={logo} alt="Sicu-aura" width={100} height={100} /> */}
+            <Button>Log Out</Button>
+        </div>
+    </nav>
+  )
+}
+
--- app/Dashboard/page.js
+++ app/Dashboard/page.js
@@ -1,15 +1,18 @@
+
 import React from 'react'
 import Head from 'next/head'
+import { Header } from './Header'
 
 export default function page () {
   return (
     <>
+        <Header />
         <Head>
             <title>Dashboard</title>
             <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         </Head>
         <main>
-    
+
         </main>
     </>
   )
