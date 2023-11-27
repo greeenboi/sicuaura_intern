c94ccd41592416aa82bec93617277fe1966f7218
greeenboi <suvan.gowrishanker.204@gmail.com>
2023-11-27 13:34:03 +0530

    Add image and search functionality to Dashboard page

--- app/Dashboard/page.js
+++ app/Dashboard/page.js
@@ -2,6 +2,16 @@
 import React from 'react'
 import Head from 'next/head'
 import { Header } from './Header'
+import Image from 'next/image'
+
+import Icon from '../../public/image 18.svg'
+
+import {
+  Button,
+  Input
+} from "@chakra-ui/react"
+import { CiSearch } from "react-icons/ci";
+import { IoFilterOutline } from "react-icons/io5";
 
 export default function page () {
   return (
@@ -11,8 +21,20 @@ export default function page () {
             <title>Dashboard</title>
             <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         </Head>
-        <main className='bg-white w-screen h-screen'>
-
+        <main className='bg-white w-screen h-screen flex flex-col items-center py-24'>
+          <div className='flex flex-row-reverse w-4/6 h-fit '>
+            <Image src={Icon} alt="Cool Logo" width={83} height={83} />
+          </div>
+          <section className='w-4/6 h-max '>
+              <div className=' flex flex-row w-full h-fit text-black justify-between'>
+                <h1 className=' font-montserrat font-medium text-4xl'>Hospital Registrations</h1>
+                <div className='flex items-center flex-row '>
+                  <Input placeholder="Search" className='w-full h-fit ml-4 p-2 border-r-0 shadow-md' size='md' />
+                  <Button className='p-0 bg-white hover:bg-white active:bg-gray-400 right-10 z-10 hover:opacity-70'  ><CiSearch /></Button>
+                  <Button className='p-0 hover:bg-white active:bg-gray-400 shadow-md mx-12' size="md" ><IoFilterOutline /></Button>
+                </div>
+              </div>
+          </section>
         </main>
     </>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,7 @@ module.exports = {
       fontFamily: {
         'poppins': ['Poppins', 'sans-serif'],
         'inter': ['Inter', 'sans-serif'],
+        'montserrat' : ['Montserrat', 'sans-serif']
       },
     },
   },
