1d803a3f9b1c8ea43cdcf93dac208e7cd0f53d8c
greeenboi <suvan.gowrishanker.204@gmail.com>
2023-11-27 13:14:34 +0530

    Add client-side code and router hook to Header component

--- app/Dashboard/Header.jsx
+++ app/Dashboard/Header.jsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React from 'react'
 import Image from 'next/image'
 import Sicu_logo1 from '../../public/sicu-aura 3.svg'
@@ -7,8 +9,11 @@ import {
     Button,
     Avatar,
 } from '@chakra-ui/react'
+import { useRouter } from 'next/navigation'
+
 
 export function Header () {
+  const router = useRouter()
   return (
     <nav className='w-full min-h-max navbar flex flex-row justify-between items-center'>
         <div className='flex flex-row mx-12'>
@@ -19,7 +24,7 @@ export function Header () {
         <div className='w-fit max-w-4xl mx-8 pl-36 flex flex-row gap-4 justify-evenly items-center'>
             <Avatar name='Dan Abrahmov' src='https://bit.ly/dan-abramov' />
             <p className='font-inter font-medium text-sm'>Alex Robinson</p>
-            <Button className=' px-4 py-2 my-2 text-black bg-slate-300 hover:bg-slate-700 hover:text-white transition-all'>Log Out</Button>
+            <Button onClick={() => { router.push('/') }} className=' px-4 py-2 my-2 text-black bg-slate-300 hover:bg-slate-700 hover:text-white transition-all'>Log Out</Button>
         </div>
     </nav>
   )
