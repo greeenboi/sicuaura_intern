8803cb789aa80a33d82c5e45c00eec6ef6cdca61
greeenboi <suvan.gowrishanker.204@gmail.com>
2023-11-27 13:38:06 +0530

    Add table component to Dashboard page

--- app/Dashboard/page.js
+++ app/Dashboard/page.js
@@ -8,7 +8,16 @@ import Icon from '../../public/image 18.svg'
 
 import {
   Button,
-  Input
+  Input,
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
 } from "@chakra-ui/react"
 import { CiSearch } from "react-icons/ci";
 import { IoFilterOutline } from "react-icons/io5";
@@ -34,6 +43,42 @@ export default function page () {
                   <Button className='p-0 hover:bg-white active:bg-gray-400 shadow-md mx-12' size="md" ><IoFilterOutline /></Button>
                 </div>
               </div>
+              <TableContainer className='text-black my-12'>
+                <Table variant='striped' colorScheme='gray'>
+                  <TableCaption>Imperial to metric conversion factors</TableCaption>
+                  <Thead>
+                    <Tr>
+                      <Th>To convert</Th>
+                      <Th>into</Th>
+                      <Th isNumeric>multiply by</Th>
+                    </Tr>
+                  </Thead>
+                  <Tbody>
+                    <Tr>
+                      <Td>inches</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td isNumeric>25.4</Td>
+                    </Tr>
+                    <Tr>
+                      <Td>feet</Td>
+                      <Td>centimetres (cm)</Td>
+                      <Td isNumeric>30.48</Td>
+                    </Tr>
+                    <Tr>
+                      <Td>yards</Td>
+                      <Td>metres (m)</Td>
+                      <Td isNumeric>0.91444</Td>
+                    </Tr>
+                  </Tbody>
+                  <Tfoot>
+                    <Tr>
+                      <Th>To convert</Th>
+                      <Th>into</Th>
+                      <Th isNumeric>multiply by</Th>
+                    </Tr>
+                  </Tfoot>
+                </Table>
+              </TableContainer>
           </section>
         </main>
     </>
