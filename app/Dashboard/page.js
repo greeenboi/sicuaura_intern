8814ca4303e67b597122655ca93e24b7ddca4f62
greeenboi <suvan.gowrishanker.204@gmail.com>
2023-11-27 14:35:45 +0530

    Add Supabase dependency and fetch hospital data

--- app/Dashboard/page.js
+++ app/Dashboard/page.js
@@ -1,5 +1,6 @@
-
+'use client'
 import React from 'react'
+import { useEffect, useState } from 'react';
 import Head from 'next/head'
 import { Header } from './Header'
 import Image from 'next/image'
@@ -18,11 +19,32 @@ import {
   Td,
   TableCaption,
   TableContainer,
+  Spinner,
 } from "@chakra-ui/react"
 import { CiSearch } from "react-icons/ci";
 import { IoFilterOutline } from "react-icons/io5";
 
+import { createClient } from '@supabase/supabase-js';
+const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+
+
 export default function page () {
+  const [hospitalData, setHospitalData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    const fetchData = async () => {
+      let { data, error } = await supabase.from('hospitalreg').select('*');
+      if (error) {
+        console.error('Error: ', error);
+        setIsLoading(false);
+      } else {
+        setHospitalData(data);
+        setIsLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <>
         <Header />
@@ -30,54 +52,64 @@ export default function page () {
             <title>Dashboard</title>
             <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         </Head>
-        <main className='bg-white w-screen h-screen flex flex-col items-center py-24'>
+        <main className='bg-white w-screen h-screen flex flex-col items-center py-24 overflow-x-hidden'>
           <div className='flex flex-row-reverse w-4/6 h-fit '>
             <Image src={Icon} alt="Cool Logo" width={83} height={83} />
           </div>
-          <section className='w-11/12 h-max '>
+          <section className='w-11/12 h-fit overflow-x-hidden'>
               <div className=' flex flex-row w-full h-fit text-black justify-between'>
                 <h1 className=' font-montserrat font-medium text-4xl'>Hospital Registrations</h1>
                 <div className='flex items-center flex-row '>
                   <Input placeholder="Search" className='w-full h-fit ml-4 p-2 border-r-0 shadow-md' size='md' />
                   <Button className='p-0 bg-white hover:bg-white active:bg-gray-400 right-10 z-10 hover:opacity-70'  ><CiSearch /></Button>
-                  <Button className='p-0 hover:bg-white active:bg-gray-400 shadow-md mx-12' size="md" ><IoFilterOutline /></Button>
+                  <Button className='p-0 hover:bg-white active:bg-gray-400 shadow-md ml-12' size="md" ><IoFilterOutline /></Button>
                 </div>
               </div>
-              <TableContainer className='text-black my-12'>
-                <Table variant='striped' colorScheme='gray'>
-                  <TableCaption>Made with 💖 by Suvan GS</TableCaption>
-                  <Thead className='bg-teal-300 border rounded-xl my-2'>
-                    <Tr >
-                      <Th>No.</Th>
-                      <Th>Date & Time</Th>
-                      <Th >Hospital Name</Th>
-                      <Th >Email</Th>
-                      <Th >Address</Th>
-                      <Th >Phone No.</Th>
-                      <Th >City</Th>
-                      <Th >State</Th>
-                      <Th >Pincode</Th>
-                    </Tr>
-                  </Thead>
-                  <Tbody>
-                    <Tr>
-                      <Td>inches</Td>
-                      <Td>inches</Td>
-                      <Td>inches</Td>
-                      <Td>inches</Td>
-                      <Td>inches</Td>
-                      <Td>inches</Td>
-                      <Td>inches</Td>
-                      <Td>millimetres (mm)</Td>
-                      <Td isNumeric>25.4</Td>
-                    </Tr>
-                    
-                  </Tbody>
-                  <Tfoot>
-                    
-                  </Tfoot>
-                </Table>
-              </TableContainer>
+              {isLoading ? 
+                (
+                  <section className='w-full h-96 flex flex-col justify-center items-center'> 
+                    <Spinner size='xl' />
+                  </ section>
+                ) : (
+                  <TableContainer className='text-black my-12'>
+                    <Table variant='striped' colorScheme='gray'>
+                      <TableCaption>Made with 💖 by Suvan GS</TableCaption>
+                      <Thead className='bg-teal-300 border rounded-xl my-2'>
+                        <Tr >
+                          <Th>No.</Th>
+                          <Th>Date & Time</Th>
+                          <Th >Hospital Name</Th>
+                          <Th >Email</Th>
+                          <Th >Address</Th>
+                          <Th >Phone No.</Th>
+                          <Th >City</Th>
+                          <Th >State</Th>
+                          <Th >Pincode</Th>
+                        </Tr>
+                      </Thead>
+                      <Tbody>
+                        
+                        {hospitalData.map((data, index) => (
+                          <Tr key={index}>
+                            <Td>{index+1}</Td>
+                            <Td>{data.date_reg}</Td>
+                            <Td>{data.hospital_name}</Td>
+                            <Td>{data.email}</Td>
+                            <Td>{data.address}</Td>
+                            <Td>{data.phone_no}</Td>
+                            <Td>{data.city}</Td>
+                            <Td>{data.state}</Td>
+                            <Td>{data.pincode}</Td>
+                          </Tr>
+                        ))}                    
+                      </Tbody>
+                      <Tfoot>
+                        
+                      </Tfoot>
+                    </Table>
+                  </TableContainer>                  
+                ) 
+              }
           </section>
         </main>
     </>
--- app/page.js
+++ app/page.js
@@ -29,24 +29,62 @@ import {
 import { LoginForm } from "./loginform";
 import Head from 'next/head';
 
+import { createClient } from '@supabase/supabase-js';
+
+const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+
 export default function Home() {
   const [isLogin, setIslogin] = useState(false);
-
+  const [isLoading, setIsLoading] = useState(false);
   const { isOpen, onOpen, onClose } = useDisclosure()
 
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
 
-  const handleSubmit = (event) => {
+
+  const [ hospital_reg , setHospital_reg ] = useState('');
+  const [hospitalName, setHospitalName] = useState('');
+  const [date_reg, setDate_reg] = useState('');
+  const [email, setEmail] = useState('');
+  const [address, setAddress] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [city, setCity] = useState('');
+  const [state, setState] = useState('');
+  const [pincode, setPincode] = useState('');
+  
+  const handleSubmit = async (event) => {
     event.preventDefault();
+    setIsLoading(true);
     if (password !== confirmPassword) {
       alert('Passwords do not match');
     } else {
-      // alert("Success");
-      onOpen();
-      setTimeout(() => {
-        onClose();
-      }, 2000);
+      console.log('Data being inserted');
+      const { data, error } = await supabase
+        .from('hospitalreg')
+        .insert([
+          { 
+            hospital_reg: hospital_reg, 
+            hospital_name: hospitalName, 
+            date_reg: date_reg, 
+            email: email, 
+            address: address, 
+            phone_no: phoneNumber, 
+            city: city, 
+            state: state, 
+            pincode: pincode 
+          },
+        ]);
+      if (error) {
+        console.error('Error: ', error);
+        setIsLoading(false);
+      } else {
+        console.log('Data inserted successfully: ', data);
+        setIsLoading(false);
+        onOpen();
+        setTimeout(() => {
+          onClose();
+        }, 2000);
+      }
     }
   };
 
@@ -90,18 +128,18 @@ export default function Home() {
             <div className={`flex flex-row gap-8 w-full  items-center justify-evenly text-black transition-all duration-500 ${isLogin ? 'opacity-0' : 'opacity-100'}`}> {/* This div will be shown when isLogin is false */}
               {/* SignUp Form */}
                 <Stack className='w-1/2 h-full gap-4'>
-                  <Input variant='flushed'  placeholder='Hospital Name' />
-                  <Input variant='flushed' placeholder='Address' />
-                  <Input variant='flushed' placeholder='City' />
-                  <Input variant='flushed' placeholder='State' />
+                  <Input variant='flushed'  placeholder='Hospital Name' value={hospitalName} onChange={(e) => setHospitalName(e.target.value)}/>
+                  <Input variant='flushed' placeholder='Address' value={address} onChange={(e) => setAddress(e.target.value)}/>
+                  <Input variant='flushed' placeholder='City' value={city} onChange={(e) => setCity(e.target.value)}/>
+                  <Input variant='flushed' placeholder='State' value={state} onChange={(e) => setState(e.target.value)}/>
                   <NumberInput variant='flushed'>
-                    <NumberInputField placeholder='Pincode' />
+                    <NumberInputField placeholder='Pincode' value={pincode} onChange={(e) => setPincode(e.target.value)} />
                     <NumberInputStepper>
                       <NumberIncrementStepper />
                       <NumberDecrementStepper />
                     </NumberInputStepper>
                   </NumberInput>
-                  <Input type='date' variant='flushed' placeholder='Hospital Registration Date' />
+                  <Input type='date' variant='flushed' placeholder='Hospital Registration Date' value={date_reg} onChange={(e) => setDate_reg(e.target.value)}/>
                   <NumberInput variant='flushed'>
                     <NumberInputField placeholder='Number of Ambulance available' />
                     <NumberInputStepper>
@@ -111,16 +149,29 @@ export default function Home() {
                   </NumberInput>
                 </Stack >
                 <Stack className='w-1/2 h-full gap-4'>
-                  <Input type='email' variant='flushed' placeholder='Email ID' />
-                  <Input type="number" variant='flushed' placeholder='Phone Number' />
-                  <Input type="number" variant='flushed' placeholder='Hospital Registration Number' />
+                  <Input type='email' variant='flushed' placeholder='Email ID' value={email} onChange={(e) => setEmail(e.target.value)}/>
+                  <Input type="number" variant='flushed' placeholder='Phone Number' value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)}/>
+                  <Input type="number" variant='flushed' placeholder='Hospital Registration Number' value={hospital_reg} onChange={(e) => setHospital_reg(e.target.value)}/>
                   <Input type="number" variant='flushed' placeholder='Emergency-Ward Number' />
                   <Button  leftIcon={<FaCloudUploadAlt />} >Registration Certificate Upload</Button>
                   <Input  variant='flushed' type="password" placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)}/>
                   <Input  variant='flushed' type='password' placeholder='Confirm Password ' value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}/>
                 </Stack >
             </div>
-            <Button type="submit" size='xl' className=' py-3 px-6 text-white bg-gray-600 hover:bg-gray-800'>Sign Up</Button>
+            {isLoading? 
+              (
+                <Button
+                  isLoading
+                  loadingText='Submitting'
+                  colorScheme='teal'
+                  variant='outline'
+                >
+                  Submit
+                </Button>
+                ) : (
+                <Button type="submit" size='xl' className=' py-3 px-6 text-white bg-gray-600 hover:bg-gray-800' >Sign Up</Button>
+              )
+          }
           </form>
       )}
       <p className='flex flex-row font-poppins text-sm text-gray-400 hover:text-sky-300 transition-all items-center hover:cursor-pointer'><LuDot />  Terms and Condition privacy policy</p>
